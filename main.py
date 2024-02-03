b01131c3573cedb69463d38bbba29d63089182f5
aviral2004 <aviralg1975@gmail.com>
2024-02-03 09:01:21 +0530

    Completed auto stitching

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import numpy as np
 import numpy.linalg as la
 import cv2
-import sys
+import skimage as sk
+import skimage.io as skio
+
+from harris import get_harris_corners, dist2
 
 def gen_mat_A(p1, p2):
     x, y = p2
@@ -25,6 +28,12 @@ def gen_full_mat_B(points1, points2):
     return b
 
 def computeH(points1, points2):
+    """
+    This function takes two sets of points and returns the homography matrix
+    that maps points2 to points1.
+
+    points1 and points2 are n x 2 arrays of corresponding points.
+    """
     A = gen_full_mat_A(points1, points2)
     B = gen_full_mat_B(points1, points2)
     mat = la.lstsq(A, B, rcond=None)[0]
@@ -102,3 +111,85 @@ def stitch_images(images):
     final_image[common] = common_image[common]
 
     return final_image
+
+def compute_mop(im, point):
+    x, y = point
+    patch = im[x-20:x+20, y-20:y+20]
+    patch = sk.transform.rescale(patch, 1/5, anti_aliasing=True)
+    patch = (patch - np.mean(patch))/np.std(patch)
+    patch = patch.flatten()
+    return patch
+
+def get_mops(im, points):
+    """
+    get the mops descriptors for the points in the image
+
+    im: the image
+    points: the points to get the mops for (n x 2 array of points)
+    """
+    # convert to grayscale
+    im = sk.color.rgb2gray(im)
+    patches = []
+    for point in points:
+        patches.append(compute_mop(im, point))
+
+    return np.array(patches)
+
+def match_patches(patches1, patches2, points1, points2, threshold=0.5):
+    """
+    match the patches in patches1 to the patches in patches2
+    """
+    dists = dist2(patches1, patches2)
+    
+    nearest_dist_index = np.argmin(dists, axis=1)
+    nearest_dist = dists[np.arange(dists.shape[0]), nearest_dist_index]
+    second_nearest_dist = np.partition(dists, 2, axis=1)[:, 1]
+
+    ratios = nearest_dist / second_nearest_dist
+    out = np.where(ratios < threshold)[0]
+
+    matched_points1 = points1[:, out]
+    matched_points2 = points2[:, nearest_dist_index[out]]
+    
+    return matched_points1, matched_points2
+
+def transform_points(h, pt):
+    """
+    transform the points using the homography matrix
+    """
+    transformed = h @ pt
+    transformed /= transformed[-1]
+    return transformed
+
+def homogenize_points(pt):
+    """
+    homogenize the points
+    """
+    return np.vstack((pt, np.ones(pt.shape[1])))
+
+def ransac(pt1, pt2, threshold=20, n_iter=5000):
+    """
+    run ransac on the points to find the best homography
+    """
+    best_h = None
+    best_inliers = 0
+
+    pt1 = homogenize_points(pt1[::-1])
+    pt2 = homogenize_points(pt2[::-1])
+
+    for _ in range(n_iter):
+        # pick 4 random points
+        idx = np.random.choice(np.arange(pt1.shape[1]), 4)
+        p1 = pt1[:, idx]
+        p2 = pt2[:, idx]
+
+        h = computeH(p1[:2].T, p2[:2].T)
+        transformed = transform_points(h, pt2)
+        error = np.linalg.norm(pt1 - transformed, axis=0)
+        inliers = np.sum(error < threshold)
+        
+        if inliers > best_inliers:
+            best_inliers = inliers
+            best_h = h
+
+    return best_h
